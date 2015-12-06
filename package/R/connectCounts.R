191f7dbb58aa622befa4b1b1c99c7d609b8b796e
Aaron Lun <alun@wehi.edu.au>
2015-12-06 18:22:50 +0000

    Switched to returning ISets with StrictGI's.

--- package/R/connectCounts.R
+++ package/R/connectCounts.R
@@ -117,7 +117,7 @@ connectCounts <- function(files, param, regions, filter=1L, type="any", second.r
 	anchors <- unlist(out.left)
 	targets <- unlist(out.right)
 	out <- InteractionSet(list(counts=out.counts), colData=DataFrame(totals=full.sizes), 
-		interactions=GInteractions(anchor1=anchors, anchor2=targets, regions=regions), 
+		interactions=GInteractions(anchor1=anchors, anchor2=targets, regions=regions, mode="strict"), 
         metadata=List(param=param))
     return(sort(out))
 }
