34ab8c02668176e61b718e5f04cd0f6f070c384e
Aaron Lun <alun@wehi.edu.au>
2016-01-12 19:20:36 +0000

    Fixed bugs in diClusters, updated docs.

--- R/diClusters.R
+++ R/diClusters.R
@@ -19,18 +19,19 @@ diClusters <- function(data.list, result.list, target, equiweight=TRUE, cluster.
 
     # Checking inputs.
 	nset <- length(data.list)
-	if (nset!=length(result.list)) { stop("indices must have same length as result list") }
+	if (nset!=length(result.list)) { stop("data list must have same length as result list") }
 	for (x in seq_len(nset)) {
-		if (!identical(length(indices[[x]]), nrow(result.list[[x]]))) {
+		if (!identical(length(data.list[[x]]), nrow(result.list[[x]]))) {
  		   	stop("corresponding entries of data and result lists must have same number of entries") 
         }
 	}
 
     # Computing the adjusted FDR for all of these samples (with equi-weighting).
+    nentries <- lengths(data.list)
     if (equiweight) {
-        weights <- rep(1/lengths(data.list), lengths(data.list))
+        weights <- rep(1/nentries, nentries)
     } else {
-        weights <- NULL
+        weights <- rep(1, sum(nentries))
     }
 
     all.ps <- list()
@@ -50,9 +51,9 @@ diClusters <- function(data.list, result.list, target, equiweight=TRUE, cluster.
     # Getting the sign.
     all.signs <- list()
     if (is.na(fc.col)) { 
-        all.signs <- lapply(lengths(data.list), logical)
+        all.signs <- lapply(nentries, logical)
     } else {
-        all.signs <- lapply(data.list, function(x) { x[,fc.col] > 0 })
+        all.signs <- lapply(result.list, function(x) { x[,fc.col] > 0 })
     }
 
 	# Controlling the cluster-level FDR.
@@ -63,8 +64,8 @@ diClusters <- function(data.list, result.list, target, equiweight=TRUE, cluster.
             pos.data.list[[x]] <- data.list[[x]][cur.sig & all.signs[[x]],]
             neg.data.list[[x]] <- data.list[[x]][cur.sig & !all.signs[[x]],]
         }
-        pos.clust <- do.call(clusterPairs, c(neg.data.list, cluster.args))
-        neg.clust <- do.call(clusterPairs, c(pos.data.list, cluster.args))
+        pos.clust <- do.call(clusterPairs, c(pos.data.list, cluster.args))
+        neg.clust <- do.call(clusterPairs, c(neg.data.list, cluster.args))
     
         # Assembling it back into a single return value.
         clust.indices <- list()
@@ -72,7 +73,7 @@ diClusters <- function(data.list, result.list, target, equiweight=TRUE, cluster.
         for (x in seq_len(nset)) {
             cur.sig <- sig[in.each.group[[x]]]
             cur.signs <- all.signs[[x]][cur.sig] 
-            full.ids <- as.integer(cur.sig)
+            full.ids <- integer(sum(cur.sig))
             full.ids[cur.signs] <- pos.clust$indices[[x]]
             full.ids[!cur.signs] <- neg.clust$indices[[x]] + additional
             clust.indices[[x]] <- full.ids
@@ -87,10 +88,9 @@ diClusters <- function(data.list, result.list, target, equiweight=TRUE, cluster.
     clusters <- FUN(sig)
 
     # Cleaning up the output.
-    sig.by.group <- split(sig, groupings)
     for (x in seq_len(nset)) {
-        full.ids <- rep(NA_integer_, data.list[[x]])
-        full.ids[sig.by.group[[x]]] <- clusters$indices[[x]]
+        full.ids <- rep(NA_integer_, nrow(data.list[[x]]))
+        full.ids[sig[in.each.group[[x]]]] <- clusters$indices[[x]]
         clusters$indices[[x]] <- full.ids
     }
     clusters$FDR <- out$FDR    
